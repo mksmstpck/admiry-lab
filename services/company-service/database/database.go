27175005215fe55a7b3b02d2382897fa25c3879f
Max Ostapchuck <mksmstpck@gmail.com>
2023-06-28 11:55:47 +0300

    feat(company-service.database): add methods for working with db

--- services/company-service/database/database.go
+++ services/company-service/database/database.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"github.com/mkskstpck/to-rename/pkg/models"
+	"github.com/pborman/uuid"
+	"github.com/uptrace/bun"
+)
+
+type CompanyDB struct {
+	database *bun.DB
+}
+
+func NewCompanyDB(database *bun.DB) *CompanyDB {
+	return &CompanyDB{
+		database: database,
+	}
+}
+
+type Companies interface {
+	CompanyFindOneById(uuid.UUID) (models.Company, int32, error)
+	CompanyFindOneByName(string) (models.Company, int32, error)
+	CompanyFindAll() ([]models.Company, int32, error)
+	CompanyCreateOne(models.Company) (int32, error)
+	CompanyUpdateOne(models.Company) (int32, error)
+	CompanyDeleteOne(uuid.UUID) (int32, error)
+}
+
+type Database struct {
+	Company Companies
+}
