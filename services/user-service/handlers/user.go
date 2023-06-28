b760a1f5ea72231526f99e11ee838de6239ea9ce
Max Ostapchuck <mksmstpck@gmail.com>
2023-06-28 11:46:08 +0300

    feat(user-service.handlers): move to bun

--- services/user-service/handlers/user.go
+++ services/user-service/handlers/user.go
@@ -3,13 +3,13 @@ package handlers
 import (
 	"context"
 
-	"github.com/google/uuid"
 	"github.com/mkskstpck/to-rename/pkg/models"
+	"github.com/pborman/uuid"
 )
 
 func (h *Handler) userReadById() {
 	h.conn.Subscribe("users-id-get", func(_, reply string, id uuid.UUID) {
-		user, code, err := h.cache.Get(id.String(), context.Background())
+		user, code, err := h.cache.GetUser(id.String(), context.Background())
 		if err == nil && user != nil {
 			res := models.Response[models.User]{Status: code, Message: user.(models.User)}
 			h.conn.Publish(reply, res)
@@ -19,7 +19,7 @@ func (h *Handler) userReadById() {
 			res := models.Response[models.User]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
 		}
-		if user.(models.User).ID == uuid.Nil {
+		if user.(models.User).ID == nil {
 			res := models.Response[models.User]{Status: 404, Error: "user not found"}
 			h.conn.Publish(reply, res)
 		}
@@ -35,7 +35,7 @@ func (h *Handler) userReadById() {
 
 func (h *Handler) userReadByUsername() {
 	h.conn.Subscribe("users-username-get", func(_, reply string, username string) {
-		user, code, err := h.cache.Get(username, context.Background())
+		user, code, err := h.cache.GetUser(username, context.Background())
 		if err == nil && user != nil {
 			res := models.Response[models.User]{Status: code, Message: user.(models.User)}
 			h.conn.Publish(reply, res)
@@ -45,7 +45,7 @@ func (h *Handler) userReadByUsername() {
 			res := models.Response[models.User]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
 		}
-		if user.(models.User).ID == uuid.Nil {
+		if user.(models.User).ID == nil {
 			res := models.Response[models.User]{Status: 404, Error: "user not found"}
 			h.conn.Publish(reply, res)
 		}
@@ -61,7 +61,7 @@ func (h *Handler) userReadByUsername() {
 
 func (h *Handler) userReadByEmail() {
 	h.conn.Subscribe("users-email-get", func(_, reply string, email string) {
-		user, code, err := h.cache.Get(email, context.Background())
+		user, code, err := h.cache.GetUser(email, context.Background())
 		if err == nil && user != nil {
 			res := models.Response[models.User]{Status: code, Message: user.(models.User)}
 			h.conn.Publish(reply, res)
@@ -71,7 +71,7 @@ func (h *Handler) userReadByEmail() {
 			res := models.Response[models.User]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
 		}
-		if user.(models.User).ID == uuid.Nil {
+		if user.(models.User).ID == nil {
 			res := models.Response[models.User]{Status: 404, Error: "user not found"}
 			h.conn.Publish(reply, res)
 		}
@@ -89,20 +89,24 @@ func (h *Handler) userCreate() {
 	h.conn.Subscribe("users-post", func(_, reply string, user models.User) {
 		userExistUsername, code, err := h.user.UserFindOneByUsername(user.Username)
 		if err != nil {
-			res := models.Response[models.User]{Status: code, Error: err.Error()}
-			h.conn.Publish(reply, res)
+			if err.Error() != "user not found" {
+				res := models.Response[models.User]{Status: code, Error: err.Error()}
+				h.conn.Publish(reply, res)
+			}
 		}
-		if userExistUsername.ID != uuid.Nil {
+		if userExistUsername.ID != nil {
 			res := models.Response[models.User]{Status: 409, Error: "user with this username already exists"}
 			h.conn.Publish(reply, res)
 			return
 		}
 		userExistEmail, code, err := h.user.UserFindOneByEmail(user.Email)
 		if err != nil {
-			res := models.Response[models.User]{Status: code, Error: err.Error()}
-			h.conn.Publish(reply, res)
+			if err.Error() != "user not found" {
+				res := models.Response[models.User]{Status: code, Error: err.Error()}
+				h.conn.Publish(reply, res)
+			}
 		}
-		if userExistEmail.ID != uuid.Nil {
+		if userExistEmail.ID != nil {
 			res := models.Response[models.User]{Status: 409, Error: "user with this email already exists"}
 			h.conn.Publish(reply, res)
 			return
