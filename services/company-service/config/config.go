e9aa603d5f5bcefd8e96042657ff459aa8a19740
Max Ostapchuck <mksmstpck@gmail.com>
2023-06-28 11:56:31 +0300

    feat(company-service.config): add config

--- services/company-service/config/config.go
+++ services/company-service/config/config.go
@@ -1,1 +1,41 @@
-package main
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	PSQLaddr     string
+	PSQLuser     string
+	PSQLpass     string
+	PSQLdb       string
+	NatsURI      string
+	RedisHost    string
+	RedisPort    string
+	RedisDB      int
+	RedisExpires int
+}
+
+func NewConfig() Config {
+	redisDBint, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if err != nil {
+		panic(err)
+	}
+	redisExpiresint, err := strconv.Atoi(os.Getenv("REDIS_EXPIRES"))
+	if err != nil {
+		panic(err)
+	}
+
+	return Config{
+		PSQLaddr:     os.Getenv("PSQL_ADDR"),
+		PSQLuser:     os.Getenv("PSQL_USER"),
+		PSQLpass:     os.Getenv("PSQL_PASS"),
+		PSQLdb:       os.Getenv("PSQL_DB"),
+		NatsURI:      os.Getenv("NATS_URI"),
+		RedisHost:    os.Getenv("REDIS_HOST"),
+		RedisPort:    os.Getenv("REDIS_PORT"),
+		RedisDB:      redisDBint,
+		RedisExpires: redisExpiresint,
+	}
+}
