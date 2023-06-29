e77c559cb54fd50a48fb3780dd989785d736c813
Max Ostapchuck <mksmstpck@gmail.com>
2023-06-29 14:05:44 +0300

    feat(api-gateway.events): add logs for events

--- services/api-gateway/events/user.go
+++ services/api-gateway/events/user.go
@@ -2,9 +2,9 @@ package events
 
 import (
 	"errors"
-	"log"
 	"time"
 
+	"github.com/labstack/gommon/log"
 	"github.com/mkskstpck/to-rename/pkg/models"
 	"github.com/pborman/uuid"
 )
@@ -13,11 +13,14 @@ func (u User) UserGetByEmail(email string) (models.User, int32, error) {
 	var res models.Response[models.User]
 	err := u.conn.Request("users-email-get", email, &res, time.Second)
 	if err != nil {
+		log.Error("events.UserGetByEmail: ", err)
 		return models.User{}, 500, err
 	}
 	if res.Error != "" {
+		log.Error("events.UserGetByEmail: ", res.Error)
 		return models.User{}, res.Status, errors.New(res.Error)
 	}
+	log.Info("events.UserGetByEmail: user found")
 	return res.Message, res.Status, nil
 }
 
@@ -25,11 +28,14 @@ func (u User) UserGetByUsername(username string) (models.User, int32, error) {
 	var res models.Response[models.User]
 	err := u.conn.Request("users-username-get", username, &res, time.Second)
 	if err != nil {
+		log.Error("events.UserGetByUsername: ", err)
 		return models.User{}, 500, err
 	}
 	if res.Error != "" {
+		log.Error("events.UserGetByUsername: ", res.Error)
 		return models.User{}, res.Status, errors.New(res.Error)
 	}
+	log.Info("events.UserGetByUsername: user found")
 	return res.Message, res.Status, nil
 }
 
@@ -37,11 +43,14 @@ func (u User) UserGetById(id uuid.UUID) (models.User, int32, error) {
 	var res models.Response[models.User]
 	err := u.conn.Request("users-id-get", id, &res, time.Second)
 	if err != nil {
+		log.Error("events.UserGetById: ", err)
 		return models.User{}, 500, err
 	}
 	if res.Error != "" {
+		log.Error("events.UserGetById: ", res.Error)
 		return models.User{}, res.Status, errors.New(res.Error)
 	}
+	log.Info("events.UserGetById: user found")
 	return res.Message, res.Status, nil
 }
 
@@ -49,11 +58,14 @@ func (u User) UserPost(user *models.User) (models.User, int32, error) {
 	var res models.Response[models.User]
 	err := u.conn.Request("users-post", user, &res, time.Second)
 	if err != nil {
+		log.Error("events.UserPost: ", err)
 		return models.User{}, 500, err
 	}
 	if res.Error != "" {
+		log.Error("events.UserPost: ", res.Error)
 		return models.User{}, res.Status, errors.New(res.Error)
 	}
+	log.Info("events.UserPost: user created")
 	return res.Message, res.Status, nil
 }
 
@@ -61,11 +73,14 @@ func (u User) UserPut(user *models.User) (int32, error) {
 	var res models.Response[string]
 	err := u.conn.Request("users-put", user, &res, time.Second)
 	if err != nil {
+		log.Error("events.UserPut: ", err)
 		return 500, err
 	}
 	if res.Error != "" {
+		log.Error("events.UserPut: ", res.Error)
 		return res.Status, errors.New(res.Error)
 	}
+	log.Info("events.UserPut: user updated")
 	return res.Status, nil
 }
 
@@ -73,12 +88,13 @@ func (u User) UserDelete(id uuid.UUID) (int32, error) {
 	var res models.Response[string]
 	err := u.conn.Request("users-delete", id, &res, time.Second)
 	if err != nil {
+		log.Error("events.UserDelete: ", err)
 		return 500, err
 	}
 	if res.Error != "" {
-		log.Println(res.Error)
+		log.Error("events.UserDelete: ", res.Error)
 		return res.Status, errors.New(res.Error)
 	}
-	log.Println(res.Error)
+	log.Info("events.UserDelete: user deleted")
 	return res.Status, nil
 }
