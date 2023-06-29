de0899371aba09c351d9d43df991b18ad3a20f9e
Max Ostapchuck <mksmstpck@gmail.com>
2023-06-29 19:06:03 +0300

    feat(company-service.handlers): add logging

--- services/company-service/handlers/company.go
+++ services/company-service/handlers/company.go
@@ -3,141 +3,185 @@ package handlers
 import (
 	"context"
 
+	"github.com/labstack/gommon/log"
 	"github.com/mkskstpck/to-rename/pkg/models"
 	"github.com/pborman/uuid"
 )
 
 func (h *Handler) companyReadById() {
-	h.conn.Subscribe("companies-id-get", func(_, reply string, id uuid.UUID) {
+	_, err := h.conn.Subscribe("companies-id-get", func(_, reply string, id uuid.UUID) {
 		company, code, err := h.cache.GetCompany(id.String(), context.Background())
 		if err == nil && company != nil {
 			res := models.Response[models.Company]{Status: code, Message: company.(models.Company)}
 			h.conn.Publish(reply, res)
+			log.Info("handlers: company found by id")
 		}
 		company, code, err = h.company.CompanyFindOneById(id)
-		if err != nil && company != nil {
+		if err != nil {
 			res := models.Response[models.Company]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Info("handlers: ", err)
 		}
 		if company.(models.Company).ID == nil {
 			res := models.Response[models.Company]{Status: 404, Error: "company not found"}
 			h.conn.Publish(reply, res)
+			log.Info("handlers: ", err)
 		}
 		code, err = h.cache.Set(id.String(), company, context.Background())
 		if err != nil {
 			res := models.Response[models.Company]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Info("handlers: ", err)
 		}
 		res := models.Response[models.Company]{Status: 200, Message: company.(models.Company)}
 		h.conn.Publish(reply, res)
+		log.Info("handlers: company found by id")
 	})
+	if err != nil {
+		log.Error("handlers: ", err)
+	}
 }
 
 func (h *Handler) companyReadByName() {
-	h.conn.Subscribe("companies-name-get", func(_, reply string, name string) {
+	_, err := h.conn.Subscribe("companies-name-get", func(_, reply string, name string) {
 		company, code, err := h.cache.GetCompany(name, context.Background())
 		if err == nil && company != nil {
 			res := models.Response[models.Company]{Status: code, Message: company.(models.Company)}
 			h.conn.Publish(reply, res)
+			log.Info("handlers: company found by name")
 		}
 		company, code, err = h.company.CompanyFindOneByName(name)
 		if err != nil {
 			res := models.Response[models.Company]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Error("handlers: ", err)
 		}
 		if company.(models.Company).ID == nil {
 			res := models.Response[models.Company]{Status: 404, Error: "company not found"}
 			h.conn.Publish(reply, res)
+			log.Info("handlers: company not found by name")
 		}
 		code, err = h.cache.Set(name, company, context.Background())
 		if err != nil {
 			res := models.Response[models.Company]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Error("handlers: ", err)
 		}
 		res := models.Response[models.Company]{Status: 200, Message: company.(models.Company)}
 		h.conn.Publish(reply, res)
+		log.Info("handlers: company found by name")
 	})
+	if err != nil {
+		log.Error("handlers: ", err)
+	}
 }
 func (h *Handler) companyReadAll() {
-	h.conn.Subscribe("companies-get", func(_, reply string, id uuid.UUID) {
+	_, err := h.conn.Subscribe("companies-get", func(_, reply string, id uuid.UUID) {
 		company, code, err := h.company.CompanyFindAll()
 		if err != nil && company != nil {
 			res := models.Response[[]models.Company]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Info("handlers: companies found")
 		}
 		if company == nil {
 			res := models.Response[[]models.Company]{Status: 404, Error: "company not found"}
 			h.conn.Publish(reply, res)
+			log.Info("handlers: companies not found")
 		}
 		res := models.Response[[]models.Company]{Status: 200, Message: company}
 		h.conn.Publish(reply, res)
+		log.Info("handlers: companies found")
 	})
+	if err != nil {
+		log.Error("handlers: ", err)
+	}
 }
 
 func (h *Handler) companyCreate() {
-	h.conn.Subscribe("companies-post", func(_, reply string, company models.Company) {
+	_, err := h.conn.Subscribe("companies-post", func(_, reply string, company models.Company) {
 		companyExist, code, err := h.company.CompanyFindOneByName(company.Name)
 		if err != nil {
 			if err.Error() != "company not found" {
 				res := models.Response[models.Company]{Status: code, Error: err.Error()}
 				h.conn.Publish(reply, res)
+				log.Error("handlers: ", err)
 			}
 		}
 		if companyExist.ID != nil {
 			res := models.Response[models.Company]{Status: 409, Error: "company with this name already exists"}
 			h.conn.Publish(reply, res)
+			log.Info("handlers: company with this name already exists")
 			return
 		}
 		code, err = h.company.CompanyCreateOne(company)
 		if err != nil {
 			res := models.Response[models.Company]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Error("handlers: ", err)
 		}
 		company, code, err = h.company.CompanyFindOneByName(company.Name)
 		if err != nil {
 			res := models.Response[models.Company]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Error("handlers: ", err)
 		}
 		code, err = h.cache.Set(company.ID.String(), company, context.Background())
 		if err != nil {
 			res := models.Response[models.Company]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Error("handlers: ", err)
 		}
 		res := models.Response[models.Company]{Status: 201, Message: company}
 		h.conn.Publish(reply, res)
+		log.Info("handlers: company created")
 	})
+	if err != nil {
+		log.Error("handlers: ", err)
+	}
 }
 
 func (h *Handler) companyUpdate() {
-	h.conn.Subscribe("companies-update", func(_, reply string, company models.Company) {
+	_, err := h.conn.Subscribe("companies-update", func(_, reply string, company models.Company) {
 		code, err := h.company.CompanyUpdateOne(company)
 		if err != nil {
 			res := models.Response[string]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Error("handlers: ", err)
 		}
 		code, err = h.cache.Set(company.ID.String(), company, context.Background())
 		if err != nil {
 			res := models.Response[string]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Error("handlers: ", err)
 		}
 		res := models.Response[string]{Status: 204, Message: "company updated"}
 		h.conn.Publish(reply, res)
+		log.Info("handlers: company updated")
 	})
+	if err != nil {
+		log.Error("handlers: ", err)
+	}
 }
 
 func (h *Handler) companyDelete() {
-	h.conn.Subscribe("companies-delete", func(_, reply string, id uuid.UUID) {
+	_, err := h.conn.Subscribe("companies-delete", func(_, reply string, id uuid.UUID) {
 		code, err := h.company.CompanyDeleteOne(id)
 		if err != nil {
 			res := models.Response[string]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Error("handlers: ", err)
 		}
 		code, err = h.cache.Delete(id.String(), context.Background())
 		if err != nil {
 			res := models.Response[string]{Status: code, Error: err.Error()}
 			h.conn.Publish(reply, res)
+			log.Error("handlers: ", err)
 		}
 		res := models.Response[string]{Status: code, Message: "company deleted"}
 		h.conn.Publish(reply, res)
+		log.Info("handlers: company deleted")
 	})
+	if err != nil {
+		log.Error("handlers: ", err)
+	}
 }
