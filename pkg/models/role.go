3955536d9851990918dd33ffde99d63ab9d3ba86
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-01 00:24:48 +0300

    feat(pkg.models): change id types

--- pkg/models/role.go
+++ pkg/models/role.go
@@ -1,7 +1,9 @@
 package models
 
+import "github.com/pborman/uuid"
+
 type Role struct {
-	ID           int32  `json:"id"`
-	Name         string `json:"name" validate:"required"`
-	PermissionID string `json:"permissions" validate:"required"`
+	ID            uuid.UUID   `json:"id" bun:"default:uuid_generate_v4() ,pk"`
+	Name          string      `json:"name" validate:"required"`
+	PermissionIDs []uuid.UUID `json:"permissions" bun:",array"`
 }
