5130c16f417b10415763aff8c66dbf9e718d69e5
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-02 22:43:32 +0300

    feat(pkg.models): add CompanyID field to role struct

--- pkg/models/role.go
+++ pkg/models/role.go
@@ -5,5 +5,6 @@ import "github.com/pborman/uuid"
 type Role struct {
 	ID            uuid.UUID   `json:"id" bun:"default:uuid_generate_v4() ,pk"`
 	Name          string      `json:"name" validate:"required"`
+	CompanyID     uuid.UUID   `json:"company_id" validate:"required"`
 	PermissionIDs []uuid.UUID `json:"permissions" bun:",array"`
 }
