2cc4567fc44718f3e67b45cb03d865e2bce6e15d
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-08 10:28:13 +0300

    feat(pkg.models): add bun's tags

--- pkg/models/permission.go
+++ pkg/models/permission.go
@@ -3,7 +3,7 @@ package models
 import "github.com/pborman/uuid"
 
 type Permission struct {
-	ID        uuid.UUID `json:"id"`
-	Name      string    `json:"name" binding:"required"`
-	AllowedTo []string  `json:"allowedTo" binding:"required"`
+	ID        uuid.UUID `json:"id" bun:"default:uuid_generate_v4() ,pk"`
+	Name      string    `json:"name" binding:"required" bun:",unique"`
+	AllowedTo []string  `json:"allowedTo" binding:"required" bun:",array"`
 }
