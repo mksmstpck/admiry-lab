c87b7da8830e669f47abbd533a5374f0f260f45d
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-15 10:05:44 +0300

    feat: add 2 fields to user model

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -3,9 +3,11 @@ package models
 import "github.com/pborman/uuid"
 
 type User struct {
-	ID       uuid.UUID `json:"id" bun:"default:uuid_generate_v1() ,pk"`
-	Username string    `json:"username" binding:"required" bun:",unique"`
-	FullName string    `json:"fullName" binding:"required" bun:",unique"`
-	Email    string    `json:"email" binding:"required" bun:",unique"`
-	Password string    `json:"password" binding:"required" bun:",notnull"`
+	ID        uuid.UUID `json:"id" bun:"default:uuid_generate_v1() ,pk"`
+	Username  string    `json:"username" binding:"required" bun:",unique"`
+	FullName  string    `json:"fullName" binding:"required" bun:",unique"`
+	Email     string    `json:"email" binding:"required" bun:",unique"`
+	Password  string    `json:"password" binding:"required" bun:",notnull"`
+	RoleID    uuid.UUID `json:"role_id" binding:"required" bun:",notnull"`
+	CompanyID uuid.UUID `json:"company_id" binding:"required" bun:",notnull"`
 }
