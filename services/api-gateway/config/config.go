450d83c8a80f5820dc1c688ea7764b586be49938
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-16 17:49:12 +0300

    feat(api-gateway.config): add few fields

--- services/api-gateway/config/config.go
+++ services/api-gateway/config/config.go
@@ -1,15 +1,42 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
 
 type Config struct {
-	NatsUrl string
-	EchoUrl string
+	NatsUrl           string
+	GinUrl            string
+	TTLAccess         time.Duration
+	TTLRefresh        time.Duration
+	PrivateRefreshKey []byte
+	PublicRefreshKey  []byte
+	PrivateAccessKey  []byte
+	PublicAccessKey   []byte
 }
 
 func NewConfig() Config {
+	ttlAccess, err := strconv.Atoi(os.Getenv("TTL_ACCESS"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ttlRefresh, err := strconv.Atoi(os.Getenv("TTL_REFRESH"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return Config{
-		NatsUrl: os.Getenv("NATS_URI"),
-		EchoUrl: os.Getenv("ECHO_URL"),
+		NatsUrl:           os.Getenv("NATS_URI"),
+		GinUrl:            os.Getenv("GIN_URL"),
+		TTLAccess:         time.Duration(ttlAccess) * time.Second,
+		TTLRefresh:        time.Duration(ttlRefresh) * time.Hour,
+		PrivateRefreshKey: []byte(os.Getenv("REFRESH_SECRET")),
+		PublicRefreshKey:  []byte(os.Getenv("REFRESH_PUBLIC")),
+		PrivateAccessKey:  []byte(os.Getenv("ACCESS_SECRET")),
+		PublicAccessKey:   []byte(os.Getenv("ACCESS_PUBLIC")),
 	}
 }
