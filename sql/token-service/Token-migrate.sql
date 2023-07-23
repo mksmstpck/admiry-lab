4376cf463901ffbb7bf1522221aab9eb27010fac
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-23 15:11:07 +0300

    feat(sql): delete token migrations

--- sql/token-service/Token-migrate.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-create table if not exists "tokens" (
-    token uuid not null,
-    expires_at integer not null,
-    user_id uuid not null,
-    primary key (token)
-
-)
