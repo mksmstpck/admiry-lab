178bcd52ef4e43d1a34b6d04316c9006def8a613
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-23 15:15:42 +0300

    feat(api-gateway.middleware): add auth middleware

--- services/api-gateway/middleware/middleware.go
+++ services/api-gateway/middleware/middleware.go
@@ -1,49 +1,36 @@
 package middleware
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/mkskstpck/admiry-lab/pkg/models"
-	"github.com/mkskstpck/admiry-lab/pkg/utils"
 	"github.com/mkskstpck/admiry-lab/services/api-gateway/events"
+	"github.com/mkskstpck/admiry-lab/services/api-gateway/jwt"
 )
 
 type Middleware struct {
-	jwt             utils.JWTs
-	user            events.Users
-	accessPublicKey []byte
+	user          events.Users
+	access_secret []byte
 }
 
-func NewMiddleware(jwt utils.JWT, users *events.User, accessPublicKey []byte) *Middleware {
+func NewMiddleware(user events.Users, access_secret []byte) *Middleware {
 	return &Middleware{
-		jwt:             jwt,
-		user:            users,
-		accessPublicKey: accessPublicKey,
+		user:          user,
+		access_secret: access_secret,
 	}
 }
 
 func (m *Middleware) DeserializeUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader := c.Request.Header.Get("Authorization")
-		if authHeader == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{Message: "Unauthorized"})
-			return
-		}
-		user_id, err := m.jwt.Validate(authHeader, m.accessPublicKey)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{Message: "Unauthorized"})
-			return
-		}
+		token := c.Request.Header.Get("Authorization")
+
+		user_id, err := jwt.ValidateJWT(token, m.access_secret)
 
 		user, code, err := m.user.UserGetById(user_id)
 		if err != nil {
 			c.AbortWithStatusJSON(int(code), models.Message{Message: err.Error()})
 			return
 		}
-
-		c.Set("user", user)
-
+		c.Set("User", user)
 		c.Next()
 	}
 }
