9c2e3326747c53462bb942b58034d3d253130fbc
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-23 15:19:35 +0300

    feat(api-gateway.config): update config

--- services/api-gateway/config/config.go
+++ services/api-gateway/config/config.go
@@ -1,42 +1,38 @@
 package config
 
 import (
-	"log"
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/labstack/gommon/log"
 )
 
 type Config struct {
-	NatsUrl           string
-	GinUrl            string
-	TTLAccess         time.Duration
-	TTLRefresh        time.Duration
-	PrivateRefreshKey []byte
-	PublicRefreshKey  []byte
-	PrivateAccessKey  []byte
-	PublicAccessKey   []byte
+	NatsUrl        string
+	GinUrl         string
+	Access_secret  []byte
+	Refresh_secret []byte
+	Access_exp     time.Duration
+	Refresh_exp    time.Duration
 }
 
 func NewConfig() Config {
-	ttlAccess, err := strconv.Atoi(os.Getenv("TTL_ACCESS"))
+	access_exp, err := strconv.Atoi(os.Getenv("ACCESS_EXP"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ttlRefresh, err := strconv.Atoi(os.Getenv("TTL_REFRESH"))
+	refresh_exp, err := strconv.Atoi(os.Getenv("REFRESH_EXP"))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	return Config{
-		NatsUrl:           os.Getenv("NATS_URI"),
-		GinUrl:            os.Getenv("GIN_URL"),
-		TTLAccess:         time.Duration(ttlAccess) * time.Second,
-		TTLRefresh:        time.Duration(ttlRefresh) * time.Hour,
-		PrivateRefreshKey: []byte(os.Getenv("REFRESH_SECRET")),
-		PublicRefreshKey:  []byte(os.Getenv("REFRESH_PUBLIC")),
-		PrivateAccessKey:  []byte(os.Getenv("ACCESS_SECRET")),
-		PublicAccessKey:   []byte(os.Getenv("ACCESS_PUBLIC")),
+		NatsUrl:        os.Getenv("NATS_URI"),
+		GinUrl:         os.Getenv("GIN_URL"),
+		Access_secret:  []byte(os.Getenv("ACCESS_SECRET")),
+		Refresh_secret: []byte(os.Getenv("REFRESH_SECRET")),
+		Access_exp:     time.Duration(access_exp),
+		Refresh_exp:    time.Duration(refresh_exp),
 	}
 }
